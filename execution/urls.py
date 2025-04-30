347ac8e2d582b6d8a7a4adc1f6d5548c6c04fe74
Diogo Toigo <dev@squadra.dev.br>
2025-04-30 18:58:22 -0300

    Add route to redirect home page to dashboard.

--- execution/urls.py
+++ execution/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/', views.executive_dashboard, name='dashboard'),
     path('dashboard/', views.executive_dashboard, name='dashboard'),
 
     # Formularios
